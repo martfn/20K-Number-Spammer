212178f74b08196998b2fba01a5d1cc5577bf94f
Martim <martim.nogueira@sapo.pt>
2022-10-27 19:41:08 +0100

    uploading files

---
+++ 20knum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 0;
+
+    while (num < 20001)
+    {
+        printf("%d\n", num);
+        num++;
+    }
+    printf("that's it.\n code by martfn. (github.com/martfn)");
+    return 0;
+}
